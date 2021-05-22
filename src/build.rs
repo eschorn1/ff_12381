62456d318dc09ed5be6d60a1b90e223db93fc11c
eschorn@integritychain.com <eschorn@integritychain.com>
2021-05-22 08:46:58 -0500

    github actions123

--- src/build.rs
+++ src/build.rs
@@ -1,5 +1,5 @@
 extern crate cc;
 
 fn main() {
-    cc::Build::new().compiler("clang").file("src/asm/mont_mul_384_asm.S").compile("ff_12381_asm");
+    cc::Build::new().file("src/asm/mont_mul_384_asm.S").compile("ff_12381_asm");
 }
